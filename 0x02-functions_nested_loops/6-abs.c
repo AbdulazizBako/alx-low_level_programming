c95a7d44a76d9d0c71da15f73026455f9dfacb7d
Abdulaziz Bako <ABako77@gmail.com>
2021-09-27 16:10:59 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - computes absolute value
+ * @a: takes an int value
+ * Return: returns absolutes value
+ *
+ */
+
+int _abs(int a)
+{
+a = abs(a);
+return (a);
+}
