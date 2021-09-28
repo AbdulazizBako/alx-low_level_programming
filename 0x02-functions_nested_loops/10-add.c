7abbc272d21548ac7e092c28c4fc025738a0f967
Abdulaziz Bako <ABako77@gmail.com>
2021-09-27 17:57:53 -0700

    sum func

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - adds two ints
+ * @a: input variable
+ * @b: input variable
+ * Return: returns ans
+ *
+ */
+int add(int a, int b)
+{
+int sum = a + b;
+return (sum);
+}
