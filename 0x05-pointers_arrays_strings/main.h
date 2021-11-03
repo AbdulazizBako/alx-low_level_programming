22adeb2f1a7300a6a5c755615a62636cb33d7d5e
Abdulaziz Bako <ABako77@gmail.com>
2021-11-03 16:21:33 -0700

    print any other number

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,11 +1,12 @@
 #ifndef SARAI_H
 #define SARAI_H
 
-int _putchar (char c);
-void reset_to_98 (int *n);
-void swap_int (int *a, int *b);
-int _strlen (char *s);
-void _puts (char *str);
-void print_rev (char *s);
-void rev_string (char *s);
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
 #endif
