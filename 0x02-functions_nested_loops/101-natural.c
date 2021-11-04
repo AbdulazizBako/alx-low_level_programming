6c41a3ffe43305a06de6b5bed661ec4b311df262
Abdulaziz Bako <ABako77@gmail.com>
2021-11-04 16:05:46 -0700

    sum

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point to a function that prints the summ of multiples of 3 & 5
+ *
+ * Return: Always 0
+ *
+ */
+int main()
+{
+int i, result;
+result = 0;
+for (i = 0; i < 1024; i++)
+{
+if ((i % 3 == 0) || (i % 5 ==0))
+{
+result += i;
+}
+}
+printf("%d", result);
+putchar('\n');
+return (0);
+}
