e0af0616fb755b29d9facb1d9a1ecfd487c20aea
Abdulaziz Bako <ABako77@gmail.com>
2021-12-15 04:35:32 -0800

    diagonals

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - diagonal lines made of backslashes
+ * @n: number of \ to be printed
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+int i;
+int j;
+
+if (n > 0)
+{
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < i; j++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
